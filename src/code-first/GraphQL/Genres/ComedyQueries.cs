c768035d3100cd0ec15d1ac5ab75f17f5d3bfb44
Sipho Hlophe <psyphore@gmail.com>
2022-02-12 12:21:11 +0200

    feat: simulate multiple data sources

---
+++ src/code-first/GraphQL/Genres/ComedyQueries.cs
@@ -0,0 +1,31 @@
+﻿using HotChocolate.Data;
+using HotChocolate.Data.Neo4J;
+using HotChocolate.Data.Neo4J.Execution;
+
+using MoviesAPI.Models;
+
+using Neo4j.Driver;
+
+namespace MoviesAPI.Schema;
+
+[ExtendObjectType(OperationTypeNames.Query)]
+public class MovieQueries
+{
+    [GraphQLName("actors")]
+    [UseNeo4JDatabase(databaseName: "neo4j")]
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    public Neo4JExecutable<Actor> GetActors(
+        [ScopedService] IAsyncSession session) =>
+        new(session);
+
+    [GraphQLName("movies")]
+    [UseNeo4JDatabase(databaseName: "neo4j")]
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    public Neo4JExecutable<Movie> GetMovies(
+        [ScopedService] IAsyncSession session) =>
+        new(session);
+}
