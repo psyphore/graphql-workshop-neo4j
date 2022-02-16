afc3365184c88a3172f00e97e820af8a50760f0a
Sipho Hlophe <psyphore@gmail.com>
2022-02-16 09:53:19 +0200

    feat: add genre

---
+++ src/code-first/GraphQL/Genres/CyberpunkMutations.cs
@@ -0,0 +1,82 @@
+﻿using HotChocolate.Data.Neo4J;
+using HotChocolate.Data.Neo4J.Execution;
+
+using LanguageExt;
+
+using MoviesAPI.Models;
+
+using Neo4j.Driver;
+
+namespace MoviesAPI.Schema.Genres;
+
+[ExtendObjectType(OperationTypeNames.Mutation)]
+public class CyberpunkMutations
+{
+    private readonly string cyberpunkgenre = nameof(cyberpunkgenre);
+
+    [GraphQLName("signActor")]
+    [UseNeo4JDatabase(nameof(cyberpunkgenre))]
+    public async ValueTask<Actor> SignActor(
+        [ScopedService] IAsyncSession session,
+        [ScopedService] CyberpunkQueries cyberpunkQueries,
+        [GraphQLNonNullType] string fullName,
+        [GraphQLNonNullType] string movie,
+        CancellationToken cancellationToken = default
+        )
+    {
+        var matched = await GetMovie(cyberpunkQueries, session, movie, cancellationToken);
+
+        var actor = new Actor
+        {
+            Name = fullName,
+        };
+
+        matched.IfSome(m => actor.ActedIn.Add(m));
+
+        Neo4JExecutable<Actor> exe = new(session);
+
+        return await ValueTask.FromResult(actor);
+    }
+
+    [GraphQLName("produceMovie")]
+    [UseNeo4JDatabase(nameof(cyberpunkgenre))]
+    public async ValueTask<Movie> ProduceMovie(
+        [ScopedService] IAsyncSession session,
+        [GraphQLNonNullType] string title
+        )
+    {
+        var movie = new Movie
+        {
+            Title = title
+        };
+
+        Neo4JExecutable<Movie> exe = new(session);
+
+        return await ValueTask.FromResult(movie);
+    }
+
+
+    private static async ValueTask<Option<Movie>> GetMovie(CyberpunkQueries cyberpunkQueries, IAsyncSession session, string movie, CancellationToken cancellationToken = default)
+    {
+        // do some stuff on neo4j
+        HotChocolate.Data.Neo4J.Language.Operator op = new(movie);
+
+        var filter = new HotChocolate.Data.Neo4J.Language.CompoundCondition(
+            op
+            );
+        var movies = cyberpunkQueries.GetMovies(session);
+
+        var matched = await movies
+            .WithFiltering(filter)
+            .WithProjection(new[] { new Movie() })
+            .FirstOrDefaultAsync(cancellationToken)
+            ;
+
+        var node = matched.As<Movie>();
+
+        return node is null ? Option<Movie>.None : node;
+    }
+
+
+
+}
---
+++ src/code-first/GraphQL/Genres/CyberpunkQueries.cs
@@ -0,0 +1,45 @@
+﻿using HotChocolate.Data;
+using HotChocolate.Data.Neo4J;
+using HotChocolate.Data.Neo4J.Execution;
+
+using MoviesAPI.Models;
+
+using Neo4j.Driver;
+
+namespace MoviesAPI.Schema.Genres;
+
+[ExtendObjectType(OperationTypeNames.Query)]
+public class CyberpunkQueries
+{
+    private readonly string cyberpunkgenre = nameof(cyberpunkgenre);
+
+    [GraphQLName("actors")]
+    [UseNeo4JDatabase(nameof(cyberpunkgenre))]
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    public Neo4JExecutable<Actor> GetActors(
+        [ScopedService] IAsyncSession session) =>
+        new(session);
+
+    [GraphQLName("movies")]
+    [UseNeo4JDatabase(nameof(cyberpunkgenre))]
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    public Neo4JExecutable<Movie> GetMovies(
+        [ScopedService] IAsyncSession session) =>
+        new(session);
+
+    [GraphQLName("movie")]
+    [UseNeo4JDatabase(nameof(cyberpunkgenre))]
+    public Neo4JExecutable<Movie> GetMovie(
+        [ScopedService] IAsyncSession session,
+        [GraphQLNonNullType] string movie)
+    {
+        Neo4JExecutable<Movie> exe = new(session);
+        HotChocolate.Data.Neo4J.Language.Operator op = new(movie);
+        var filter = new HotChocolate.Data.Neo4J.Language.CompoundCondition(op);
+        return (Neo4JExecutable<Movie>)exe.WithFiltering(filter);
+    }
+}
