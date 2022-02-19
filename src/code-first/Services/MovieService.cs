2de7bfb3ce4d41d52b6833feccb520e8de2be7ff
Sipho Hlophe <psyphore@gmail.com>
2022-02-19 16:09:23 +0200

    feat: move crud actions into services

---
+++ src/code-first/Services/MovieService.cs
@@ -0,0 +1,71 @@
+﻿
+using LanguageExt;
+
+using MoviesAPI.Executables;
+using MoviesAPI.Extensions;
+using MoviesAPI.Models;
+
+using Neo4j.Driver;
+
+namespace MoviesAPI.Services;
+
+public class MovieService
+{
+    private readonly IDriver _driver;
+
+    public MovieService(IDriver driver) => _driver = driver;
+
+    public async ValueTask<IEnumerable<Movie>> GetMovies(string dbname, CancellationToken cancellationToken = default)
+    {
+        var exe = new FlexExecutable<Movie>(_driver, dbname);
+        var movies = await exe.MaterializeNodes<Movie>(cancellationToken: cancellationToken);
+        return movies;
+    }
+
+    public async ValueTask<Option<Movie>> GetMovie(string name, string dbname, CancellationToken cancellationToken = default)
+    {
+        var filter_op = new HotChocolate.Data.Neo4J.Language.Operator(name);
+
+        var exe = new FlexExecutable<Movie>(_driver, dbname)
+            .WithFiltering(new HotChocolate.Data.Neo4J.Language.CompoundCondition(filter_op));
+
+        var movie = await exe.MaterializeNode<Movie>(cancellationToken: cancellationToken);
+        return movie is null ? Option<Movie>.None : movie;
+    }
+
+    public async ValueTask<Movie> UpdateMovie(string title, string dbname, CancellationToken cancellationToken = default)
+    {
+        var movie = new Movie
+        {
+            Title = title
+        };
+
+        var exists = await GetMovie(title, dbname, cancellationToken);
+        exists.IfSome(m => movie = m);
+
+        var exe = new FlexExecutable<Movie>(_driver, dbname);
+        var result = await exe.UpdateAsync(movie, cancellationToken);
+
+        return movie;
+    }
+
+    public async ValueTask<Movie> CastActor(string title, string actor, string dbname, CancellationToken cancellationToken = default)
+    {
+        var movie_exe = new FlexExecutable<Movie>(_driver, dbname);
+
+        var movie = new Movie
+        {
+            Title = title
+        };
+
+        var star = new Person
+        {
+            Name = actor
+        };
+
+        movie.Actors.Add(star);
+
+        return await Task.FromResult(movie);
+    }
+
+}
---
+++ src/code-first/Services/PersonService.cs
@@ -0,0 +1,50 @@
+﻿
+using LanguageExt;
+
+using MoviesAPI.Executables;
+using MoviesAPI.Extensions;
+using MoviesAPI.Models;
+
+using Neo4j.Driver;
+
+namespace MoviesAPI.Services;
+
+public class PersonService
+{
+    private readonly IDriver _driver;
+
+    public PersonService(IDriver driver) => _driver = driver;
+
+    public async ValueTask<IEnumerable<Person>> GetActors(string dbname, CancellationToken cancellationToken = default)
+    {
+        var exe = new FlexExecutable<Person>(_driver, dbname);
+        var actors = await exe.MaterializeNodes<Person>(cancellationToken);
+        return actors;
+    }
+
+    public async ValueTask<Option<Person>> GetActor(string name, string dbname, CancellationToken cancellationToken = default)
+    {
+        var filter_op = new HotChocolate.Data.Neo4J.Language.Operator(name);
+        var exe = new FlexExecutable<Person>(_driver, dbname)
+            .WithFiltering(new HotChocolate.Data.Neo4J.Language.CompoundCondition(filter_op));
+
+        var actor = await exe.MaterializeNode<Person>(cancellationToken);
+        return actor is null ? Option<Person>.None : actor;
+    }
+
+    public async ValueTask<Person> CreateActor(string name, string dbname, CancellationToken cancellationToken = default)
+    {
+        Person star = new()
+        {
+            Name = name,
+        };
+
+        var actor = await GetActor(name, dbname, cancellationToken);
+        actor.IfSome(actor => star = actor);
+
+        var exe = new FlexExecutable<Person>(_driver, dbname);
+        var result = await exe.UpdateAsync(star, cancellationToken);
+
+        return star;
+    }
+}
